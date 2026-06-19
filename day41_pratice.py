33c201e365d6e3a19a64a045b5170d7ffbbd5eeb
Lyrakun <273812235+Lyrakunai@users.noreply.github.com>
2026-06-19 13:38:48 +0530

    Add files via upload

---
+++ day41_pratice.py
@@ -0,0 +1,34 @@
+# ==========================================
+# Codesaga Day 41
+# Strings Foundation & Python Thinking
+# ==========================================
+
+# String literals
+print("Hello")
+print("Patna")
+print("Python")
+
+# Empty string
+print("")
+
+# Space string
+print(" ")
+
+# Variables and strings
+city = "Patna"
+name = "Nainu"
+language = "Python"
+
+print(city)
+print(name)
+print(language)
+
+# Valid string expression
+"Python"
+
+# Variable lookup
+city
+
+# More examples
+message = "Learning Strings"
+print(message)
